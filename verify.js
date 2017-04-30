06e67c2058209eae145b5b8ff90179efb2c327f8
Shikhir Arora <shikhir@thesarogroup.com>
2017-04-29 20:14:35 -0400

    Updated with better docs, tested node v7.0.0+, example usage

--- verify.js
+++ verify.js
@@ -1,9 +1,19 @@
-"use_strict"; // project compiled in nodejs v6.1.0 or higher
+/**
 
-const TOKEN = 'TOKEN HERE'; // OAuth2 Bot Token
-const GUILD = 'GUILD ID'; // guild ID
-const ROLE = 'ROLE ID'; // role to assign once verified
-const VERIFY_MESSAGE = 'I agree to the rules and my token is {token}'; // edit this accordingly, leaving {token} 
+The following strings can all be edited very easily. 
+Specifically, lines 37-45 contains our implementation of the messages.
+These are fully flexible and can be edited to whatever you wish to accomplish with the bot. :-)
+The use case here is just an example and can act as a boilerplate for some.
+
+**/
+
+'use_strict'; // tested for node v7.0 or higher
+
+const TOKEN = ''; // Bot users only. Your OAuth2 bot token will go here (not client ID). 
+const GUILD = '';  // The ID of the guild this bot will be in. Must be the guild ID, not the guild name.
+const ROLE = '';  // The role ID that the bot will set upon successful verification below. The role ID must be given, not the role name. 
+const VERIFY_MESSAGE = 'I agree to the rules and my token is {token}'; // leave {token} but edit the message accordingly if you wish, this is the string the members must paste to gain access 
+const GAME_PLAYING = 'Playing...'; // Edit this to the game you want shown that the bot will have as its playing status
 
 const Discord = require('discord.js');
 const shortcode = (n) => {
@@ -16,21 +26,31 @@ const shortcode = (n) => {
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`[CLIENT] Ready as ${client.user.username}#${client.user.discriminator} ${client.user.id}`);
+  client.user.setGame(GAME_PLAYING);
+  console.log(`[VERIFYBOT] Connected as ${client.user.username}#${client.user.discriminator} ${client.user.id}`);
 });
 
 client.on('guildMemberAdd', (member) => {
   if (member.user.bot || member.guild.id !== GUILD) return;
-  const token = shortcode(7);
+  const token = shortcode(9); // we choose a 9 digit token here. can be any integer number. 
   console.log(`${member.user.username}#${member.user.discriminator} joined! CODE: "${token}"`);
-  member.send(`Welcome to the server. To verify your account and agree to our rules, type: **I agree to the rules and my token is** \`${token}\` This will verify your account. Thank you!`);  // edit this line to modify the bot's PM to the user with instructions when they first join
+  member.send(`Welcome to the server! **PLEASE TAKE A MOMENT TO READ OUR RULES HERE:** [website, rules, etc. can go here] - and once you have read and agree to the policies, send a reply here as a private DM message with the following single line: 
+  
+  
+  I agree to the rules and my token is \`${token}\` 
+  
+  
+  Once you copy and send this private message to me (please ensure the message is exactly as written above as it is case-sensitive), your role will automatically be updated to \`MEMBERS\` (edit this accordingly) and you will be able to view all the rooms & history (you may need to refresh your Discord client). This will be kept as your written verification of your understanding & acceptance of the server rules and ensures a smooth user experience for everybody. 
+  
+  *This process is fully automatic, but if you need any assistance feel free to please post in the main room.* As a final reminder: your usage of this server is contingent on understanding these rules and also the general Discord Terms of Service/Privacy Policies which are linked on our rules page (edit accordingly). Thank you for your understanding and stopping by :slight_smile: `);
+  
   member.user.token = token;
 });
 
 client.on('message', (message) => {
-  if (message.author.bot || !message.author.token) return; // reverted requirement that the verify message must be a PM; to change add || message.channel.type !== 'dm'  
+  if (message.author.bot || !message.author.token || message.channel.type !== 'dm') return;
   if (message.content !== VERIFY_MESSAGE.replace('{token}', message.author.token)) return;
-  message.channel.send('Thank you for verifying your account. Your roles have been updated and you can now view all user channels.'); // edit this to modify what the bot sends to the user when they verify their account 
+  message.channel.send('Thank you for accepting the rules and verifying your account. Your account has been automatically assigned the **[ROLE NAME: EDIT ACCORDINGLY]** role, allowing you to view our member rooms and history. Please refresh Discord if you cannot view the history in the main room and everything should appear. Thank you again for taking this brief step to verify your account with (server name - edit accordingly)! We hope you enjoy your time here!');
   const role = client.guilds.get(GUILD).roles.get(ROLE);
   client.guilds.get(GUILD).member(message.author.id).addRole(role).catch(console.error);
 });
