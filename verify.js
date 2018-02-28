337f748938b6e5d37823742d2ef3152067918f5f
Shikhir Arora <shikhir@thesarogroup.com>
2018-02-28 07:04:41 -0500

    Make this work with discord.js v12.0-dev xD
    As I should have spent the 5 minutes to do so earlier (but no one is
    looking 👀)…
    
    -Just some minor syntax change(s) to work with the GuildMemberRoleStore
    in discord.js master branch (v12.0-dev of the library)
    
    -Added the shrug emoji to the package.json description in order to
    emotionally convey the simple nature of this bot. Good stuff!

--- verify.js
+++ verify.js
@@ -6,65 +6,67 @@ const config = require('./config.json')
 const completemsg = `Thank you for agreeing to the rules and code of conduct! You are now a verified member of the guild! \nFeel free to choose what roles you’d like, introduce yourself or check out a our other channels. \n\n**Your unique token is your signature that you have read and understood our rules.**\n`
 
 const shortcode = (n) => {
-  const possible = 'ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghjklmnopqrstuvwxyz0123456789'
-  let text = ''
-  for (var i = 0; i < n + 1; i++) text += possible.charAt(Math.floor(Math.random() * possible.length))
-  return text;
+    const possible = 'ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghjklmnopqrstuvwxyz0123456789'
+    let text = ''
+    for (var i = 0; i < n + 1; i++) text += possible.charAt(Math.floor(Math.random() * possible.length))
+    return text;
 }
-
 
 const client = new Discord.Client()
 
 client.on('ready', () => {
-  client.user.setActivity(config.playing)
-  console.log(`[VERIFYBOT] Connected as ${client.user.username}#${client.user.discriminator} ${client.user.id}`)
+    client.user.setActivity(config.playing)
+    console.log(`[VERIFYBOT] Connected as ${client.user.username}#${client.user.discriminator} ${client.user.id}`)
 })
 
 client.on('guildMemberAdd', (member) => {
-  if (member.user.bot || member.guild.id !== config.guild) return
-  const token = shortcode(8)
-  const welcomemsg = `Welcome to the guild! We hope you find a home here! Check out the \`#general\` channel to make sure that we jive, and as long as our goals are similar, then there’s a place at the table waiting for you. \n\n If you accept the code of conduct, please verify your agreement by replying to **this DM** with the verification phrase: \n\n\`I agree to abide by all rules. My token is ${token}.\`\n\n **This message is case-sensitive, and please include the period at the end! ** \n\nQuestions? Get at a staff member in the server or via DM.`
-  console.log(`${member.user.username}#${member.user.discriminator} joined! CODE: "${token}"`)
-  member.send(welcomemsg)
-  member.user.token = token
+    if (member.user.bot || member.guild.id !== config.guild) return
+    const token = shortcode(8)
+    const welcomemsg = `Welcome to the guild! We hope you find a home here! Check out the \`#general\` channel to make sure that we jive, and as long as our goals are similar, then there’s a place at the table waiting for you. \n\n If you accept the code of conduct, please verify your agreement by replying to **this DM** with the verification phrase: \n\n\`I agree to abide by all rules. My token is ${token}.\`\n\n **This message is case-sensitive, and please include the period at the end! ** \n\nQuestions? Get at a staff member in the server or via DM.`
+    console.log(`${member.user.username}#${member.user.discriminator} joined! CODE: "${token}"`)
+    member.send(welcomemsg)
+    member.user.token = token
 })
 
-const verifymsg = 'I agree to abide by all rules. My token is {token}.'
+const verifymsg = `I agree to abide by all rules. My token is ${token}.`
 
 client.on('message', (message) => {
-  if (message.author.bot || !message.author.token || message.channel.type !== 'dm') return
-  if (message.content !== (verifymsg.replace('{token}', message.author.token))) return
-  message.channel.send({embed: {
+    if (message.author.bot || !message.author.token || message.channel.type !== `dm`) return
+    if (message.content !== (verifymsg.replace(`${token}`, message.author.token))) return
+    message.channel.send({
+        embed: {
             color: Math.floor(Math.random() * (0xFFFFFF + 1)),
             description: completemsg,
-            timestamp: new Date()
-          }
-        })
-  const role = client.guilds.get(config.guild).roles.get(config.role)
-  client.guilds.get(config.guild).member(message.author.id).addRole(role).catch(console.error)
-  console.log(`TOKEN: ${message.author.token} :: Role ${config.role} added to member ${message.author.id}`)
+            timestamp: new Date(),
+            footer: {
+                text: `Verification Success`
+            }
+        }
+    })
+    client.guilds.get(config.guild).member(message.author).roles.add([config.role])
+        .then(console.log(`TOKEN: ${message.author.token} :: Role ${config.role} added to member ${message.author.id}`))
+        .catch(console.error)
 })
 
-
 client.on('disconnect', (event) => {
-  setTimeout(() => client.destroy().then(() => client.login(config.token)), 10000)
-  console.log(`[DISCONNECT] Notice: Disconnected from gateway with code ${event.code} - Attempting reconnect.`)
+    setTimeout(() => client.destroy().then(() => client.login(config.token)), 10000)
+    console.log(`[DISCONNECT] Notice: Disconnected from gateway with code ${event.code} - Attempting reconnect.`)
 })
 
 client.on('reconnecting', () => {
-  console.log(`[NOTICE] ReconnectAction: Reconnecting to Discord...`)
+    console.log(`[NOTICE] ReconnectAction: Reconnecting to Discord...`)
 })
 
 client.on('error', console.error)
 client.on('warn', console.warn)
 
 process.on('unhandledRejection', (error) => {
-  console.error(`Uncaught Promise Error: \n${error.stack}`)
+    console.error(`Uncaught Promise Error: \n${error.stack}`)
 })
 
 process.on('uncaughtException', (err) => {
-  let errmsg = (err ? err.stack || err : '').toString().replace(new RegExp(`${__dirname}/`, 'g'), './')
-  console.error(errmsg)
+    let errmsg = (err ? err.stack || err : '').toString().replace(new RegExp(`${__dirname}/`, 'g'), './')
+    console.error(errmsg)
 })
 
 client.login(config.token)
